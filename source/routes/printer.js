8df302f6030bd32f6ff2324de4f262123b788130
Cyese <87186561+Cyese@users.noreply.github.com>
2023-12-04 00:42:16 +0700

    relocate stuff

--- source/routes/printer.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const router = express.Router();
-
-const printerController = require('../controllers/printerController');
-
-router.get('/', printerController.print);
-
-module.exports = router;
