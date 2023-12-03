8e1f35f180a397e2c3970f816d0bf278c1baf235
Vo Mai Phuong <maiphuongbrt9a1@gmail.com>
2023-12-03 13:29:23 +0700

    chỉnh sửa vị trí các button, thêm các icon vào button và chỉnh sửa lại khung drop_zone

--- src/pages/Print/index.js
+++ src/pages/Print/index.js
@@ -1,26 +1,33 @@
 import React from 'react';
 import './StyleForPrintForm.css';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faTrash, faPrint, faEye, faFile } from '@fortawesome/free-solid-svg-icons';
+
 function Print() {
   return (
     <div className="main">
-        <button id="choosePrint" >Choose print</button>
-        <div id="myList" style={{display: 'none'}}>
-            <select id="mySelect">
-                <option value="1">Mục 1</option>
-                <option value="2">Mục 2</option>
-                <option value="3">Mục 3</option>
-                <option value="4">Mục 4</option>
-                <option value="5">Mục 5</option>
-            </select>
-            <button >Đóng</button>
+        <div id="choosePrint_ChooseFile_DeleteFile_ButtonGroup"> 
+            <button id="choosePrint" > <FontAwesomeIcon icon={faPrint}/> Choose print </button>
+            <div id="myList" style={{display: 'none'}}>
+                <select id="mySelect">
+                    <option value="1">Mục 1</option>
+                    <option value="2">Mục 2</option>
+                    <option value="3">Mục 3</option>
+                    <option value="4">Mục 4</option>
+                    <option value="5">Mục 5</option>
+                </select>
+                <button >Close</button>
+            </div>
+            <input type="file" id="myfile"></input>
+            <button id="deleteFile"> <FontAwesomeIcon icon={faTrash} /> Delete file</button>
         </div>
-        <input type="file" id="myfile"></input>
-        <button id="deleteFile">Delete file</button>
         <div id="drop_zone" style={{width: '1005px', height:'405px', padding:'10px', border:'1px solid #aaaaaa'}} draggable="true">
             Drop files here <br/>
         </div>
-        <button id = "preview">Preview</button>
-        <button id = "print">Print</button>  
+        <div id="previewAndPrintButtonGroup"> 
+            <button id = "preview"> <FontAwesomeIcon icon={faEye}/> Preview</button>
+            <button id = "print"> <FontAwesomeIcon icon={faPrint}/> Print</button>  
+        </div>
     </div>
 );
 }
